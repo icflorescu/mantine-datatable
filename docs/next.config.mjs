61595848c0c460af966944d8f657900f3bf08829
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-08-28 00:30:35 +0300

    Trying to fix pages deployment

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -3,14 +3,23 @@ import pkg from '../package/package.json' assert { type: 'json' };
 
 const withTM = transpileModules(['mantine-datatable']);
 
-/**
- * @type {import('next').NextConfig}
- */
-const config = {
-  reactStrictMode: true,
-  env: {
-    PACKAGE_VERSION: pkg.version,
-  },
+const nextConfig = (phase) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const config = {
+    reactStrictMode: true,
+    env: {
+      PACKAGE_VERSION: pkg.version,
+    },
+  };
+
+  if (phase === 'phase-production-build' && process.env.GITHUB_PAGES === 'true') {
+    config.basePath = '/mantine-datatable';
+    config.trailingSlash = true;
+  }
+
+  return withTM(config);
 };
 
-export default withTM(config);
+export default nextConfig;
