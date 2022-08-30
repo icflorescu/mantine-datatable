6696fc0398abfa1799c0147d21c8e178546d4395
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-08-31 00:00:00 +0300

    Customize browser scrollbars

--- docs/components/AppWrapper.tsx
+++ docs/components/AppWrapper.tsx
@@ -27,6 +27,28 @@ export default function AppWrapper({ children }: { children: ReactNode }) {
             background:
               theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.fn.lighten(theme.colors.gray[0], 0.9),
           },
+          '::-webkit-scrollbar': {
+            width: 12,
+            height: 12,
+            background: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[3],
+          },
+          '::-webkit-scrollbar-thumb': {
+            background: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[5],
+            borderRadius: 6,
+            border: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[3]}`,
+            '&:hover': {
+              background: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[6],
+            },
+          },
+          '@-moz-document url-prefix()': {
+            '*': {
+              scrollbarWidth: 'thin',
+              scrollbarColor:
+                theme.colorScheme === 'dark'
+                  ? `${theme.colors.dark[6]} ${theme.colors.dark[4]}`
+                  : `${theme.colors.gray[3]} ${theme.colors.gray[5]}`,
+            },
+          },
         })}
       />
       {navbarVisible && (
