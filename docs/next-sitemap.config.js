f9d917fe3f0484a424a79956a161df6e9a0ea506
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-09-02 16:32:19 +0300

    Adding docs sitemap & some build hooks

---
+++ docs/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://icflorescu.github.io/mantine-datatable/',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
