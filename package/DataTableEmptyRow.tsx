838aa8eca69a58c2cc23154ac35b4fc425dbd7c5
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-12-03 01:11:50 +0200

    Update deps & trying to fix empty row btm border

--- package/DataTableEmptyRow.tsx
+++ package/DataTableEmptyRow.tsx
@@ -4,6 +4,9 @@ const useStyles = createStyles({
   root: {
     '&&': {
       background: 'transparent',
+      ':last-of-type td': {
+        borderBottom: 'none',
+      },
     },
   },
 });
@@ -12,7 +15,7 @@ export default function DataTableEmptyRow() {
   const { classes } = useStyles();
   return (
     <tr className={classes.root}>
-      <td></td>
+      <td />
     </tr>
   );
 }
