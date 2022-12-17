f8f081a18da397319016a5005e3fd0f0ed1cb01d
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-12-17 21:26:58 +0200

    Remove console.log & adjust row styling example

--- docs/examples/AdditionalStylingExampleWithRowStyling.tsx
+++ docs/examples/AdditionalStylingExampleWithRowStyling.tsx
@@ -5,17 +5,17 @@ import companies from '~/data/companies.json';
 const records = companies.slice(0, 5);
 
 // example-start
-const useStyles = createStyles({
-  boldRow: { fontWeight: 'bold' },
-});
+const useStyles = createStyles((theme) => ({
+  blueishRow: { color: theme.colors.blue[6] },
+}));
 
-export default function AdditionalStylingExampleWithRowClassName() {
+export default function AdditionalStylingExampleWithRowStyling() {
   const { classes } = useStyles();
   return (
     <DataTable
       withBorder
-      rowClassName={({ name }) => (name === 'Hettinger, Willms and Connelly' ? classes.boldRow : undefined)}
-      rowStyle={({ name }) => (name === 'Champlin - Spencer' ? { color: 'red' } : undefined)}
+      rowClassName={({ name }) => (name === 'Hettinger, Willms and Connelly' ? classes.blueishRow : undefined)}
+      rowStyle={({ name }) => (name === 'Champlin - Spencer' ? { color: '#FA5639' } : undefined)}
       // example-skip
       columns={[
         { accessor: 'name' },
