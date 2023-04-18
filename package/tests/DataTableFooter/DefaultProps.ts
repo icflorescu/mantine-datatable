0abbf0e4bc6e865e09f7cb960dd79e8bfcf466fb
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-04-18 14:22:45 +0300

    Update footer test

--- package/tests/DataTableFooter/DefaultProps.ts
+++ package/tests/DataTableFooter/DefaultProps.ts
@@ -9,6 +9,14 @@ export const defaultProps = {
   noRecordsText: 'No records found',
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   paginationText: ({ from, to, totalRecords }: any) => `Showing ${from}-${to} of ${totalRecords} records`,
+  getPaginationControlProps: (control: 'previous' | 'next') => {
+    if (control === 'previous') {
+      return { 'aria-label': 'Previous page' };
+    } else if (control === 'next') {
+      return { 'aria-label': 'Next page' };
+    }
+    return {};
+  },
   totalRecords: 100,
   recordsPerPage: 10,
   onRecordsPerPageChange: jest.fn(),
