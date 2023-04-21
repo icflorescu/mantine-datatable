e2439d888326ac9d38fd83fcdc05f7d823e0e5a6
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-04-21 20:56:26 +0300

    Docs website nav styling tweaks

--- docs/components/AppNavbarLinkListItem.tsx
+++ docs/components/AppNavbarLinkListItem.tsx
@@ -1,47 +1,60 @@
-import { Box, createStyles, MantineColor, Text, UnstyledButton } from '@mantine/core';
+import { Box, createStyles, getStylesRef, MantineColor, Text, UnstyledButton } from '@mantine/core';
 import Link from 'next/link';
 
-const useStyles = createStyles((theme, { color = 'blue' }: { color?: MantineColor }) => ({
-  root: {
-    display: 'block',
-    transition: 'background .15s ease',
-    '&:hover': {
-      background: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[1],
+const useStyles = createStyles((theme, { color = 'blue' }: { color?: MantineColor }) => {
+  const hoverBackground = theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[1];
+  const pressedBackground = theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3];
+
+  return {
+    root: {
+      display: 'block',
+      transition: 'background .15s ease',
+      '&:hover': {
+        background: hoverBackground,
+        [`.${getStylesRef('bullet')}`]: { borderColor: hoverBackground },
+      },
+      '&:active': {
+        background: pressedBackground,
+        [`.${getStylesRef('bullet')}`]: { borderColor: pressedBackground },
+      },
     },
-    '&:active': {
-      background: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3],
+    active: {
+      background: theme.fn.rgba(theme.colors[color][6], theme.colorScheme === 'dark' ? 0.2 : 0.1),
+      '&:hover': {
+        background:
+          theme.colorScheme === 'dark'
+            ? theme.fn.rgba(theme.fn.lighten(theme.colors[color][6], 0.1), 0.2)
+            : theme.fn.rgba(theme.fn.darken(theme.colors[color][6], 0.1), 0.15),
+        [`.${getStylesRef('bullet')}`]: { borderColor: hoverBackground },
+      },
+      '&:active': {
+        background:
+          theme.colorScheme === 'dark'
+            ? theme.fn.rgba(theme.fn.lighten(theme.colors[color][6], 0.3), 0.2)
+            : theme.fn.rgba(theme.fn.darken(theme.colors[color][6], 0.3), 0.15),
+        [`.${getStylesRef('bullet')}`]: { borderColor: pressedBackground },
+      },
     },
-  },
-  active: {
-    background: theme.fn.rgba(theme.colors[color][6], theme.colorScheme === 'dark' ? 0.2 : 0.1),
-    '&:hover': {
-      background:
-        theme.colorScheme === 'dark'
-          ? theme.fn.rgba(theme.fn.lighten(theme.colors[color][6], 0.1), 0.2)
-          : theme.fn.rgba(theme.fn.darken(theme.colors[color][6], 0.1), 0.15),
+    bullet: {
+      ref: getStylesRef('bullet'),
+      flex: '0 0 16px',
+      height: 16,
+      margin: '3px 0 0 -2px',
+      background: theme.colors[color][6],
+      border: `2px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[0]}`,
+      transition: 'border-color .15s ease',
+      borderRadius: '50%',
+      zIndex: 1,
     },
-    '&:active': {
-      background:
-        theme.colorScheme === 'dark'
-          ? theme.fn.rgba(theme.fn.lighten(theme.colors[color][6], 0.3), 0.2)
-          : theme.fn.rgba(theme.fn.darken(theme.colors[color][6], 0.3), 0.15),
+    content: {
+      display: 'flex',
+      gap: 16,
     },
-  },
-  content: {
-    display: 'flex',
-    gap: 16,
-  },
-  bullet: {
-    flex: '0 0 12px',
-    height: 12,
-    marginTop: 5,
-    background: theme.colors[color][6],
-    borderRadius: '50%',
-  },
-  text: {
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[7],
-  },
-}));
+    text: {
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[7],
+    },
+  };
+});
 
 type AppNavbarLinkListItemProps = {
   title: string;
