072d133701441b57a8b981f58bb4b89513f77fb2
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-05-07 13:08:20 +0300

    Rebuild docs website yarn.lock

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -84,7 +84,6 @@ export default function _App(props: AppProps) {
           <Notifications />
           <ModalsProvider>
             <AppWrapper>
-              {/* @ts-expect-error 'Component' cannot be used as a JSX component. */}
               <Component {...pageProps} />
             </AppWrapper>
           </ModalsProvider>
