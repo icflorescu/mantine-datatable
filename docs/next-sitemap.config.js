2e7dfe0b4610f9d53ee5dbee2874ac53b4cc53b3
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-05-12 20:19:03 +0300

    Update docs building

--- docs/next-sitemap.config.js
+++ docs/next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
+  outDir: 'out',
   siteUrl: 'https://icflorescu.github.io/mantine-datatable/',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
