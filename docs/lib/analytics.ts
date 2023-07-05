6cd720e26aadf17a9991f22b666485ec02542ad1
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-07-05 13:24:15 +0300

    Remove mixpanel

--- docs/lib/analytics.ts
+++
@@ -1,8 +0,0 @@
-import mixpanel from 'mixpanel-browser';
-import { MIXPANEL_PROJECT_TOKEN } from '~/config';
-
-mixpanel.init(MIXPANEL_PROJECT_TOKEN, { debug: process.env.NODE_ENV === 'development' });
-
-export function logPageView() {
-  mixpanel.track(location.pathname);
-}
