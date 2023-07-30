31643a2018b5ada8afff0e1a7f7f0f15555cad6b
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-07-30 12:11:18 +0300

    Update docs website homepage

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -62,8 +62,9 @@ export default function Page() {
           <img src={`${process.env.BASE_PATH}/mantine-datatable.png`} alt="Mantine DataTable supports dark mode" />
         </div>
         <Text className={classes.subtitle}>
-          Mantine DataTable is a lightweight, dependency-free component that brings datagrid-like functionality to your
-          data-rich user interfaces. Features{' '}
+          Mantine DataTable is a{' '}
+          <ExternalLink to="https://bundlephobia.com/package/mantine-datatable">lightweight</ExternalLink>,
+          dependency-free component that brings datagrid-like functionality to your data-rich user interfaces. Features{' '}
           <InternalLink to="/examples/row-context-menu">context-menu support</InternalLink> and{' '}
           <InternalLink to="/examples/records-selection">
             intuitive Gmail-style additive batch rows selection
