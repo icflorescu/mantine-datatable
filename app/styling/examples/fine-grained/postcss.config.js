9ea1cd609dc2ee5f924dfd87e94c3300826dabe0
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-10-24 11:54:45 +0300

    Docs: update example comments

--- app/styling/examples/fine-grained/postcss.config.js
+++ app/styling/examples/fine-grained/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     // 👇 the postcss-import plugin must come first;
-    //    make sure to install it with `npm i -D postcss-import` or `yarn add -D postcss-import`
+    //    make sure to install it (`npm i -D postcss-import`)
     'postcss-import': {},
     'postcss-preset-mantine': {},
     'postcss-simple-vars': {
