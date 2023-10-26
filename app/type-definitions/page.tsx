8767a15afc85be7030cc5bf1ad2d1985a830d8e6
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-10-26 16:31:27 +0300

    Docs: implement type definitions page

--- app/type-definitions/page.tsx
+++ app/type-definitions/page.tsx
@@ -1,16 +1,25 @@
+import { readdir } from 'node:fs/promises';
 import { PRODUCT_NAME } from '~/app/config';
 import { CodeBlock } from '~/components/CodeBlock';
 import { PageNavigation } from '~/components/PageNavigation';
 import { PageTitle } from '~/components/PageTitle';
 import { Txt } from '~/components/Txt';
 import { readCodeFile } from '~/lib/code';
-import { getRouteMetadata } from '~/lib/utils';
+import { allPromiseProps, getRouteMetadata } from '~/lib/utils';
 
 const PATH = '/type-definitions';
 export const metadata = getRouteMetadata(PATH);
 
+const TYPE_FILES_PATH = 'package/types';
+
 export default async function TypeDefinitionsPage() {
-  const code = (await readCodeFile(`/../package/types.ts`)) as string;
+  const filesNames = await readdir(TYPE_FILES_PATH);
+
+  const files = await Promise.all(
+    filesNames
+      .filter((name) => name !== 'index.ts')
+      .map((name) => allPromiseProps({ [name]: readCodeFile(`/../${TYPE_FILES_PATH}/${name}`) }))
+  );
   return (
     <>
       <PageTitle of={PATH} />
@@ -18,8 +27,11 @@ export default async function TypeDefinitionsPage() {
         {PRODUCT_NAME} is written in TypeScript and the component properties are well documented with additional JSDoc
         annotations, so you can harness the full power of your IDE to build type safe applications with confidence.
       </Txt>
-      <Txt>Have a look at the types below to grasp the full power of {PRODUCT_NAME}:</Txt>
-      <CodeBlock code={code} language="ts" />
+      <Txt>Here are the {PRODUCT_NAME} type files:</Txt>
+      {files.map((code) => {
+        const [name] = Object.keys(code);
+        return <CodeBlock key={name} tabs={{ code, keys: [name] }} />;
+      })}
       <PageNavigation of={PATH} />
     </>
   );
