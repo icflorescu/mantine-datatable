b0a7b393b00a2b101fbf5d0bc362838053fd76ea
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-10-30 22:16:41 +0200

    Docs: add package users

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -7,6 +7,7 @@ import classes from './AppWrapper.module.css';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { Navbar } from './Navbar';
+import { PackageUsers } from './PackageUsers';
 
 export function AppWrapper({ children }: React.PropsWithChildren) {
   const [navbarExpanded, { toggle: toggleNavbar, close: collapseNavbar }] = useDisclosure(false);
@@ -28,6 +29,7 @@ export function AppWrapper({ children }: React.PropsWithChildren) {
       <AppShellMain className={classes.main}>
         <div className={classes.content}>
           <Container>{children}</Container>
+          <PackageUsers />
         </div>
       </AppShellMain>
       <Footer ref={ref} />
