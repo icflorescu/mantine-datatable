0be1e0699b582b2715eaa0f7d12c4e1ee3538f6f
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-10-31 13:16:04 +0200

    Docs: fix logo

--- components/HeaderTitle.tsx
+++ components/HeaderTitle.tsx
@@ -1,5 +1,5 @@
 import { Title } from '@mantine/core';
-import { IconPointer } from '@tabler/icons-react';
+import { IconTable } from '@tabler/icons-react';
 import Link from 'next/link';
 import { PRODUCT_NAME } from '~/app/config';
 import classes from './HeaderTitle.module.css';
@@ -7,7 +7,7 @@ import classes from './HeaderTitle.module.css';
 export function HeaderTitle() {
   return (
     <Link className={classes.root} href="/">
-      <IconPointer className={classes.icon} />
+      <IconTable className={classes.icon} />
       <Title className={classes.text} order={1}>
         {PRODUCT_NAME}
       </Title>
