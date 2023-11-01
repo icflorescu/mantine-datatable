9173126a53da11393527a6e3d410b45d5d2aed04
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-11-01 15:02:31 +0200

    Docs: more SEO improvements

--- app/manifest.webmanifest/route.ts
+++ app/manifest.webmanifest/route.ts
@@ -23,16 +23,37 @@ const data: MetadataRoute.Manifest = {
       src: `${urlPrefix}/icon.svg`,
       type: 'image/svg+xml',
       sizes: 'any',
+      purpose: 'any',
+    },
+    {
+      src: `${urlPrefix}/icon.svg`,
+      type: 'image/svg+xml',
+      sizes: 'any',
+      purpose: 'maskable',
     },
     {
       src: `${urlPrefix}/icon-192.png`,
       type: 'image/png',
       sizes: '192x192',
+      purpose: 'any',
+    },
+    {
+      src: `${urlPrefix}/icon-192.png`,
+      type: 'image/png',
+      sizes: '192x192',
+      purpose: 'maskable',
     },
     {
       src: `${urlPrefix}/icon-512.png`,
       type: 'image/png',
       sizes: '512x512',
+      purpose: 'any',
+    },
+    {
+      src: `${urlPrefix}/icon-512.png`,
+      type: 'image/png',
+      sizes: '512x512',
+      purpose: 'maskable',
     },
   ],
 };
