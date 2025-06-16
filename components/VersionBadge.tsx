d850e03857d1fa090930d86476b11c1dd659f137
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2025-06-16 15:35:38 +0300

    Update deps, README, bump version

--- components/VersionBadge.tsx
+++ components/VersionBadge.tsx
@@ -28,9 +28,9 @@ export function VersionBadge() {
             <IconAlertSquareRoundedFilled />
           </Box>
           <br />
-          This version works with Mantine V8.1
+          This version works with Mantine V8.x
           <br />
-          (and probably V7).
+          (and probably V7.x).
           <br />
           <br />
           If you’re still using <ExternalLink to="https://v6.mantine.dev">Mantine V6</ExternalLink>, check the old
